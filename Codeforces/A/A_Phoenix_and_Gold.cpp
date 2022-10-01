3483a142179453ad2c385a6a496db9ad66774eb5
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-10-01 23:53:56 +0600

    Add files via upload

---
+++ Codeforces/A/A_Phoenix_and_Gold.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int ans = 0;
+        map<char, int> mp;
+        for (int i = 0; i < s.size(); i++)
+        {
+            mp[s[i]]++;
+            char z = s[i];
+            while (s[i] == z)
+            {
+                i++;
+            }
+            i--;
+            if (mp[z] > 1)
+                ans = 1;
+        }
+        if (ans == 1)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
