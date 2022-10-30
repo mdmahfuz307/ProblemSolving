d9a709db68960ee1dfe1a63c59281a00bc4eb0d7
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-10-30 10:20:54 +0600

    Add files via upload

---
+++ Codeforces/A/A_Do_Not_Be_Distracted.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int ans = 0;
+        map<char, int> mp;
+        for (int i = 0; i > size();i++)
+        {
+        }
+    }
+}
