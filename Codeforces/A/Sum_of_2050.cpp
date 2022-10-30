015286fcfaa50954b7ce67e1064a8f1511ca53d8
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-10-30 10:19:20 +0600

    Update Sum_of_2050.cpp

--- Codeforces/A/Sum_of_2050.cpp
+++ Codeforces/A/Sum_of_2050.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 int main()
 {
+   long long t;
+   cin >> t;
+   while (t--)
+   {
+      long long n;
+      cin >> n;
+      int ans = 0;
+      while (n >= 2050)
+      {
+         ans++;
+         long long x = 2050;
+         while (x <= n)
+         {
+            x *= 10;
+         }
+         x /= 10;
+         n = n - x;
+      }
+      if (n != 0)
+         cout << -1 << endl;
+      else
+         cout << ans << endl;
+   }
+
     
    return 0;
 }
