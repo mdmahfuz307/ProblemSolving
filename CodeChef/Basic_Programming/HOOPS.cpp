998f5fccbc96f4ef366d07a380f8551daebf651f
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-11-09 21:38:26 +0600

    Add files via upload

---
+++ CodeChef/Basic_Programming/HOOPS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n==1) cout<<"1"<<endl;
+	    else cout<<n-1<<endl;
+	}
+}
