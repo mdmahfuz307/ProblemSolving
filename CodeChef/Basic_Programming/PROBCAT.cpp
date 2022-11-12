60d8326e6b52bdc81d7d7bc0ecb3086a24ac0ff4
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-11-12 20:04:20 +0600

    Add files via upload

---
+++ CodeChef/Basic_Programming/PROBCAT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=1 && x<100) cout<<"Easy"<<endl;
+	    else if(x>=2 && x<200) cout<<"Medium"<<endl;
+	    else cout<<"Hard"<<endl;
+	    
+	}
+	return 0;
+}
