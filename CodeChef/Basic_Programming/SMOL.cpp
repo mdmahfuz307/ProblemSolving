feb415e561f2f1f1e875640e44cc2fd8404b328e
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-11-12 21:12:14 +0600

    Add files via upload

---
+++ CodeChef/Basic_Programming/SMOL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n,k; cin>>n>>k;
+	    if(k==0) cout<<n<<endl;
+	    else cout<<n%k<<endl;
+	    
+	}
+	return 0;
+}
