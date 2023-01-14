e560cabf474ab7099f8c9c5bda693e31dbe11f93
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-01-14 21:31:19 +0600

    Add files via upload

---
+++ Codeforces/A/791A.cpp
@@ -0,0 +1,15 @@
+// Bear and Big Brother
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b,i;
+    cin >> a >> b;
+    for (i = 1;;i++)
+    {
+        a *= 3;
+        b *= 2;
+        if(a>b)break;
+    }
+    cout << i;
+}
