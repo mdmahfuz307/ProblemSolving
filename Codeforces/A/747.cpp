eaefce9791ac1725fea9391d5e990826fd4810b0
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-01-16 23:37:01 +0600

    Add files via upload

---
+++ Codeforces/A/747.cpp
@@ -0,0 +1,15 @@
+// Consecutive Sum Riddle
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        cout << -(n - 1) << " " << n << endl;
+    }
+    return 0;
+}
