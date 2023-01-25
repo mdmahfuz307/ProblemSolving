9678abdc3696ab9d5a3d5dcf8eafe903a8b8b05b
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-01-25 23:46:31 +0600

    Add files via upload

---
+++ Codeforces/A/41A.cpp
@@ -0,0 +1,19 @@
+//Translation
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, a;
+    cin >> s;
+    cin >> a;
+    reverse(s.begin(), s.end());
+    if(s==a)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
