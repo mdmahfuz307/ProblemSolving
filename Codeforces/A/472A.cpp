39dbd6f7b8eb2258db27c32677f04bd7719c5346
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-01-28 23:23:09 +0600

    Add files via upload

---
+++ Codeforces/A/472A.cpp
@@ -0,0 +1,17 @@
+// Design Tutorial: Learn from Math
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    if(t%2==0)
+    {
+        cout << "4"<< " " << t - 4;
+    }
+    else
+        {
+            cout << "9"<< " " << t - 9 << endl;
+             }
+    
+}
