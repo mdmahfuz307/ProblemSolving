0f5060b9c307bfb678f795c82d39f059833d5c92
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-02-17 21:10:28 +0600

    Add files via upload

---
+++ LeetCode/Study Plan/Binary Search/Binary Search I/852_Peak_Index_in_a_Mountain_Array.cpp
@@ -0,0 +1,25 @@
+/* 852. Peak Index in a Mountain Array
+Question :- https://leetcode.com/problems/peak-index-in-a-mountain-array/description/ */
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int s = 0;
+        int e = arr.size() - 1;
+        int mid = s + (e - s) / 2;
+        while (s < e)
+        {
+            if (arr[mid] < arr[mid + 1])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return s;
+    }
+};
