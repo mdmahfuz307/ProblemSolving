ffbcf0e9a44101499711f6ced6d8aaf8257fbab0
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-02-25 22:29:14 +0600

    Add files via upload

---
+++ Codeforces/A/43A _Football.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int x = 0, y = 0, z = 0;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '1')
+        {
+            x++;
+            y = 0;
+        }
+        else
+        {
+            y++;
+            x = 0;
+        }
+        if (x == 7 || y == 7)
+        {
+            z = 1;
+        }
+    }
+    if (z == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
