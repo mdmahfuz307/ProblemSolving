753109c2c9b72a04c8ef8f7da393e3f5ee1c3bed
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-05-18 12:13:44 +0600

    Add files via upload

---
+++ CodeChef/TOP10.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef double dl;
+
+#define pb push_back
+#define endl "\n"
+#define ar array
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+void solve()
+{
+    ll a;
+    cin>>a;
+    
+    ll b = 10;
+    
+    if(a<=b) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+}
+
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _ = 1;
+    cin >> _;
+    while (_--)
+        solve();
+    return 0;
+}
