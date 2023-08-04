1acbcd167234b07815e0f41dd09bebee464cce9e
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-08-04 18:02:24 +0600

    B_Memo_and_Momo.cpp

--- Codeforces/Div_2/B_Memo_and_Momo.cpp
+++ Codeforces/Div_2/B_Memo_and_Momo.cpp
@@ -24,19 +24,13 @@ int main()
     ll a, b, k;
     cin >> a >> b >> k;
     if (a%k==0 && b%k==0)
-    {
         cout << "Both" << endl;
-    }
     else if (a % k == 0 && b % k != 0)
-    {
-        cout << "Memo";
-    }
+        cout << "Memo"<<endl;
     else if (a % k != 0 && b % k == 0)
-    {
-        cout << "Momo";
-    }
-    else
-        cout << "No One";
+        cout << "Momo"<<endl;
+     else
+        cout << "No One"<<endl;
 
     return 0;
 }
