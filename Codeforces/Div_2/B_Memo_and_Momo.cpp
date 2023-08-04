9b4c754d1e303762aace6c75be49ecda7f9eb533
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-08-04 17:59:34 +0600

    B_Memo_and_Momo.cpp

---
+++ Codeforces/Div_2/B_Memo_and_Momo.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> v;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef double dl;
+
+const ll mx = 1e5 + 123;
+int ar[mx];
+
+#define pb push_back
+#define endl "\n"
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a, b, k;
+    cin >> a >> b >> k;
+    if (a%k==0 && b%k==0)
+    {
+        cout << "Both" << endl;
+    }
+    else if (a % k == 0 && b % k != 0)
+    {
+        cout << "Memo";
+    }
+    else if (a % k != 0 && b % k == 0)
+    {
+        cout << "Momo";
+    }
+    else
+        cout << "No One";
+
+    return 0;
+}
