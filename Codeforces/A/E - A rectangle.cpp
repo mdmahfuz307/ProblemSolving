016fd6545df5c296ba0b69ec25f37d59c65f3cc1
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-11-19 01:07:23 +0600

    E - A rectangle.cpp

---
+++ Codeforces/A/E - A rectangle.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double dl;
+typedef long long ll;
+typedef vector<int> v;
+typedef vector<ll> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef unsigned long long ull;
+
+#define endl "\n"
+#define pb push_back
+#define MOD 1000000007
+#define maximum INT_MAX
+#define sqr(a) ((a) * (a))
+#define PI 3.141592653589793238462
+
+#define max3(a, b, c) max(c, max(a, b))
+#define min3(a, b, c) min(c, min(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+
+const ll mx = 1e5 + 123;
+ll ar[mx];
+
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    ll x = c - a;
+    ll y = d - b;
+
+    ll ans = 0;
+
+    ans += (y / 2 + 1) * (x / 2 + 1);
+    ans += (y / 2) * ((x - 2) / 2 + 1);
+
+    cout << ans;
+
+    /* ll n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &x : v)
+        cin >> x;
+    */
+    return 0;
+}
