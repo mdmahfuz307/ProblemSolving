ecd9b0b4c891a79f40b7ff77a2272f2b7d6c8ec7
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-12-09 23:02:10 +0600

    A - Omkar and Bad Story.cpp

---
+++ Codeforces/A/A - Omkar and Bad Story.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+
+#define int long long
+using namespace std;
+
+signed main() {
+    int q; cin >> q;
+    while (q--) {
+        int n; cin >> n;
+        vector<int> a(n);
+        bool b = false; int mx = 0;
+        for (auto &x:a) {
+            cin >> x;
+            if (x < 0) {
+                b = true;
+            }
+            mx = max(mx, x);
+        }
+        if (b) {
+            cout << "nO\n";
+        } else {
+            cout << "yes\n" << mx + 1 << '\n';
+            for (int i = 0; i <= mx ; ++i) cout << i << ' ';
+            cout << '\n';
+        }
+    }
+}
