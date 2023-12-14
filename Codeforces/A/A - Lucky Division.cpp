5df9021fdd783b8bad44efca57905b368b5b13d6
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-12-14 23:27:16 +0600

    A - Lucky Division.cpp

---
+++ Codeforces/A/A - Lucky Division.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%4==0 || a%7==0 || a%47==0 || a%744==0 || a%477==0)
+        cout<<"YES";
+    else
+        cout<<"NO";
+}
