74e5253e775f40a8f3185b56372c37c069e424ea
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-01-05 23:12:43 +0600

    A - 2023.cpp

---
+++ Codeforces/A/A - 2023.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int q = 0; q < t; ++q) {
+        int n, k;
+        int64_t pr = 1;
+        cin >> n >> k;
+        vector<int64_t> v(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> v[i];
+            pr *= v[i];
+        }
+        if (int64_t(2023) % pr == 0) {
+            cout << "YES\n";
+            cout << int64_t(2023) / pr;
+            for (int i = 1; i < k; ++i) cout << " 1";
+            cout << "\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+    }
+    return 0;   
+}
