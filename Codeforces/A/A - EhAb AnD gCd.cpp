5f6c12a4f9b1b92f952e3c38eb9ddbcc5417ee6d
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-01-06 23:16:23 +0600

    A - EhAb AnD gCd.cpp

---
+++ Codeforces/A/A - EhAb AnD gCd.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        cout << 1 << " " << x-1 << '\n';
+    }
+ 
+    return 0;
+}
