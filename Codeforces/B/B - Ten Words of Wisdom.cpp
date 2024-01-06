a5cc70b5af1228e3d63beca211a68c2d5bdd667c
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-01-06 23:16:57 +0600

    B - Ten Words of Wisdom.cpp

---
+++ Codeforces/B/B - Ten Words of Wisdom.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double dl;
+typedef long long ll;
+typedef vector<int> v;
+typedef vector<ll> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef unsigned long long ull;
+
+#define endl "\n"
+#define pb push_back
+#define MOD 1000000007
+#define maximum INT_MAX
+#define sqr(a) ((a) * (a))
+#define PI 3.141592653589793238462
+#define all(n) (n).begin(), (n).end()
+
+#define max3(a, b, c) max(c, max(a, b))
+#define min3(a, b, c) min(c, min(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+
+const ll mx = 1e5 + 123;
+ll ar[mx];
+
+ll gcd(ll a, ll b)
+{
+    return __gcd(a, b);
+}
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+void solve()
+{
+    int a, b, i, t1;
+    cin >> t1;
+    int qua = 0, pos = 0;
+    for (i = 1; i <= t1; i++)
+    {
+        cin >> a >> b;
+        if (b > qua && a <= 10)
+        {
+            qua = b;
+            pos = i;
+        }
+    }
+    cout << pos << endl;
+
+    // {
+    //     f = true;
+    // }
+    // if(f)
+    //     cout << "NO" << endl;
+    //     else
+    //     cout << "YES" << endl;
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int _ = 1;
+    cin >> _;
+    while (_--)
+        solve();
+    return 0;
+}
