463a3210a4ee0a08e919374804b7ac10f9acef41
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-20 23:30:42 +0600

    Div 2

--- Codeforces/A/A. Zhan's Blender.cpp
+++ Codeforces/A/A. Zhan's Blender.cpp
@@ -5,29 +5,25 @@ typedef double dl;
 typedef long long ll;
 typedef vector<int> v;
 typedef vector<ll> vll;
-typedef map<int, int> mp;
 typedef pair<int, int> ii;
+typedef vector<ii> vii;
 typedef unsigned long long ull;
 
-#define fi first
 #define endl "\n"
-#define se second
 #define pb push_back
 #define MOD 1000000007
 #define maximum INT_MAX
 #define sqr(a) ((a) * (a))
-#define ha cout<<"YES"<<endl;
-#define na cout<<"NO"<<endl;
+#define PI 3.141592653589793238462
 #define all(n) (n).begin(), (n).end()
-#define rall(a) (a).rbegin(),(a).rend()
-#define mas(n) for (int i = 0; i < n; i++)
-#define nir(arr) {for (auto & x: arr) cin >>x;}
-#define PI 3.1415926535897932384626433832795028841971
 
 #define max3(a, b, c) max(c, max(a, b))
 #define min3(a, b, c) min(c, min(a, b))
 #define max4(a, b, c, d) max(d, max(c, max(a, b)))
 #define min4(a, b, c, d) min(d, min(c, min(a, b)))
+
+const ll mx = 1e5 + 123;
+ll ar[mx];
 
 ll gcd(ll a, ll b)
 {
@@ -37,22 +33,15 @@ ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
 
 mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
-const ll mx = 1e5 + 123;
-ll ar[mx];
-
 void solve()
 {
-    
-    int x, y;
-        cin >> x >> y; 
-        
-        cout << min(x, y) << " " << max(x, y) << endl;
+    ll n,x,y;
+    cin>>n>>x>>y;
 
+    int min_f = min(x,y);
 
-
-
-    // vector<int>ai(b);
-    //  for (auto & i : ai)cin >> i;
+    int sec = ceil((double)n / min_f);
+    cout<<sec<<endl;
 
     // {
     //     f = true;
