1cab1b31db7c324c22a275417bd922b7c185f8d2
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-25 20:49:24 +0600

    Div 4

---
+++ CodeChef/Div-4/A/AI is Coming.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double dl;
+typedef long long ll;
+typedef vector<int> v;
+typedef vector<ll> vll;
+typedef map<int, int> mp;
+typedef pair<int, int> ii;
+typedef unsigned long long ull;
+
+#define fi first
+#define endl "\n"
+#define se second
+#define pb push_back
+#define MOD 1000000007
+#define maximum INT_MAX
+#define sqr(a) ((a) * (a))
+#define all(n) (n).begin(), (n).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define mas(n) for (int i = 0; i < n; i++)
+#define nir(arr) {for (auto & x: arr) cin >>x;}
+#define PI 3.1415926535897932384626433832795028841971
+
+#define max3(a, b, c) max(c, max(a, b))
+#define min3(a, b, c) min(c, min(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int X;
+    cin >> X;
+
+
+    if (X <= 60) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    /* ll n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &x : v)
+    cin >> x;
+*/
+    return 0;
+}
