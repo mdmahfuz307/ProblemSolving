424effd9310d993b40e987b01dbe33d3a54934b0
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-14 23:42:33 +0600

    Div 2

--- Codeforces/A/A. Two Screens.cpp
+++ Codeforces/A/A. Two Screens.cpp
@@ -5,22 +5,17 @@ typedef double dl;
 typedef long long ll;
 typedef vector<int> v;
 typedef vector<ll> vll;
-typedef map<int, int> mp;
 typedef pair<int, int> ii;
+typedef vector<ii> vii;
 typedef unsigned long long ull;
 
-#define fi first
 #define endl "\n"
-#define se second
 #define pb push_back
 #define MOD 1000000007
 #define maximum INT_MAX
 #define sqr(a) ((a) * (a))
+#define PI 3.141592653589793238462
 #define all(n) (n).begin(), (n).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define mas(n) for (int i = 0; i < n; i++)
-#define nir(arr) {for (auto & x: arr) cin >>x;}
-#define PI 3.1415926535897932384626433832795028841971
 
 #define max3(a, b, c) max(c, max(a, b))
 #define min3(a, b, c) min(c, min(a, b))
@@ -38,25 +33,25 @@ ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
 
 mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
+void solve()
+{
+    
+    // {
+    //     f = true;
+    // }
+    // if(f)
+    //     cout << "NO" << endl;
+    //     else
+    //     cout << "YES" << endl;
+}
+
 int main()
 {
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
-
-    ll n;
-    cin>>n;
-
-    for(int i =  0;i<n;i++){
-        cin>>ar[i];
-    }
-
-    
-
-    /* ll n;
-    cin >> n;
-    vector<int> v(n);
-    for(auto &x : v)
-    cin >> x;
-*/
+    int _ = 1;
+    cin >> _;
+    while (_--)
+        solve();
     return 0;
 }
