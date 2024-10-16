d23a649adc366a50d487970f420ee5fcb9325c0d
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-16 21:17:35 +0600

    DIV - 4

---
+++ CodeChef/Div-4/A/Sweets Shop.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long x,n;
+	cin>>x>>n;
+	
+	long long laddus = 10 * n;
+	
+	long long aca = x - laddus;
+	
+	long long jalebi = aca / 20;
+	
+	cout<<jalebi<<endl;
+	
+
+}
